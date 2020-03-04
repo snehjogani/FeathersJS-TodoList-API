799492f61a1e0f3e026f393699e30423af38c336
Sneh Jogani <sjogani16@gmail.com>
2020-03-04 16:07:08 -0400

    package-json updated

--- src/mongoose.js
+++ src/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function (app) {
   mongoose.connect(app.get('mongodb'), {
+    useNewUrlParser: true
     // useMongoClient: true
   });
   mongoose.Promise = global.Promise;
